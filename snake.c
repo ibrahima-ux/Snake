822fafc2bcfeed5b5512d6bcd6922967de3b0fda
mike-djoco <djoco.mike@outlook.com>
2023-11-21 17:23:06 +0100

    le debut

--- snake.c
+++ snake.c
@@ -21,24 +21,78 @@ int main(void) {
     */
     
     InitialiserGraphique();
-    CreerFenetre(10,10,900,500);
+    int windowsMargin = 10;
+    int nbligne = 40, nbcolone = 60;
+    int taillecase = 20;
+    int windowsH = taillecase*nbligne + windowsMargin*3, windowsL = taillecase*nbcolone + windowsMargin*2;
+    CreerFenetre(10,10,windowsL,windowsH);
 
     ChoisirCouleurDessin(CouleurParComposante(0, 0, 0));
-    RemplirRectangle(0,0,900,500);
+    RemplirRectangle(0,0,windowsL,windowsH);
 
     couleur Background;
     Background=CouleurParComposante(38, 132, 0);
     ChoisirCouleurDessin(Background);
-    RemplirRectangle(10,10,880,400);
+    RemplirRectangle(windowsMargin ,windowsMargin ,windowsL-2*windowsMargin ,windowsH - windowsMargin*3 -TailleSupPolice(2));
 
     ChoisirCouleurDessin(CouleurParComposante(255, 255, 255));
-    int y = 80 - TailleSupPolice(2) + 410;
-    EcrireTexte(15,y,"01:27",2);
-    char[8] textes = "0000190";
-    int x = 980 - TailleChaineEcran(textes,2);
+    int y = windowsMargin*2 +TailleSupPolice(2) - TailleSupPolice(2) + windowsH-windowsMargin*3 -TailleSupPolice(2);
+    y = windowsH - windowsMargin;
+    EcrireTexte(windowsMargin +10 ,y ,"01:27" ,2);
+    char textes[8] = "0000190";
+    int x = windowsL - windowsMargin - TailleChaineEcran(textes,2);
     EcrireTexte(x,y,textes,2);
 
-    Touche();
+
+
+
+    int posHeadX=20, posHeadY=25;
+    int taileSnake = 10;
+    int plateau[60][40] = {0};
+    for (int i = posHeadY; i < taileSnake; i++){
+        int empl = 1;
+        if (i == posHeadY){
+            plateau[posHeadX][i]=1;
+        }else if(empl == taileSnake){
+            plateau[posHeadX][i]=3;
+        }else{
+            plateau[posHeadX][i]=2;
+        }
+        empl++;
+    }
+
+    int snakehead, snaketail, numbody=0;
+    int snakebody[25];
+    snakehead = ChargerSprite("./snakeHead.png");
+	AfficherSprite(snakehead,posHeadY*taillecase+windowsMargin,posHeadX*taillecase+windowsMargin);
+    for (int i = posHeadY; i < taileSnake; i++){
+        int empl = 1;
+        if (i == posHeadY){
+            snakehead = ChargerSprite("./snakeHead.png");
+            AfficherSprite(snakehead,i*taillecase+windowsMargin,posHeadX*taillecase+windowsMargin);
+        }else if(empl == taileSnake){
+            snaketail = ChargerSprite("./snakeTail.png");
+            AfficherSprite(snaketail,i*taillecase+windowsMargin,posHeadX*taillecase+windowsMargin);
+        }else{
+            snakebody[numbody]=ChargerSprite("./snakeBody.png");
+            AfficherSprite(snakebody[numbody],i*taillecase+windowsMargin,posHeadX*taillecase+windowsMargin);
+            numbody++;
+        }
+        empl++;
+    }
+    
+
+    int touche;
+    while(touche!=XK_Escape){
+        
+
+    	if (ToucheEnAttente()){
+    		touche=Touche();
+    	}
+    }
+
+
+
     FermerGraphique();
     return EXIT_SUCCESS;
 }
